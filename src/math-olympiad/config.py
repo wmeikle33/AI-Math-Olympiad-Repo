c3e1addc83ae7a3bba5cf9978f9fe8a0af5b3c9f
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-02 15:00:57 +0800

    Update config.py

--- src/math-olympiad/config.py
+++ src/math-olympiad/config.py
@@ -1,2 +1,9 @@
 Class Config:
-  
+    notebook_limit = (4 * 60 + 55) * 60  # 4h 55m
+    high_problem_timeout = 900
+    base_problem_timeout = 300
+    server_timeout = 180
+    session_timeout = 960
+    jupyter_timeout = 15
+    sandbox_timeout = 5
+
