3f7d274d70409a12bf34fdf1669a6ebbfb92dbfd
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-03 20:28:41 +0800

    Update predict.py

--- src/math-olympiad/predict.py
+++ src/math-olympiad/predict.py
@@ -1,13 +1,25 @@
-def predict(id_: pl.DataFrame, question: pl.DataFrame, answer: Optional[pl.DataFrame] = None) -> pl.DataFrame:
+def main():
+    def predict(id_: pl.DataFrame, question: pl.DataFrame, answer: Optional[pl.DataFrame] = None) -> pl.DataFrame:
+        
+        id_value = id_.item(0)
+        question_text = question.item(0)
+        
+        gc.disable()
+        
+        final_answer = solver.solve_problem(question_text)
+        
+        gc.enable()
+        gc.collect()
+        
+        return pl.DataFrame({'id': id_value, 'answer': final_answer})
     
-    id_value = id_.item(0)
-    question_text = question.item(0)
+    inference_server = kaggle_evaluation.aimo_3_inference_server.AIMO3InferenceServer(predict)
     
-    gc.disable()
-    
-    final_answer = solver.solve_problem(question_text)
-    
-    gc.enable()
-    gc.collect()
-    
-    return pl.DataFrame({'id': id_value, 'answer': final_answer})
+    if os.getenv('KAGGLE_IS_COMPETITION_RERUN'):
+        inference_server.serve()
+        
+    else:
+        inference_server.run_local_gateway(
+            ('./reference.csv',)
+        )
+        
