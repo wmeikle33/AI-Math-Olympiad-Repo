268fabf785e12621901e6242572d706ea4d29524
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-03 19:57:04 +0800

    Update train.py

--- src/math-olympiad/train.py
+++ src/math-olympiad/train.py
@@ -1,3 +1,36 @@
+import gc
+import re
+import math
+import time
+import queue
+import threading
+import contextlib
+from typing import Optional
+from jupyter_client import KernelManager
+from collections import Counter, defaultdict
+from concurrent.futures import as_completed, ThreadPoolExecutor
+import json
+import pandas as pd
+import polars as pl
+
+from openai import OpenAI
+
+from openai_harmony import (
+    HarmonyEncodingName, 
+    load_harmony_encoding, 
+    SystemContent, 
+    ReasoningEffort, 
+    ToolNamespaceConfig, 
+    Author, 
+    Message, 
+    Role, 
+    TextContent, 
+    Conversation
+)
+
+from transformers import set_seed, AutoTokenizer
+import kaggle_evaluation.aimo_3_inference_server
+
 set_env(
     input_archive='/kaggle/input/notebooks/shelterw/aimo-3-vllm-v16/wheels.tar.gz', 
     temp_dir='/kaggle/tmp/setup'
