95bfb094f81894e90a76e8c51c9c49e7c37ca001
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-03 20:25:30 +0800

    Update predict.py

--- src/math-olympiad/predict.py
+++ src/math-olympiad/predict.py
@@ -1,1 +1,13 @@
-
+def predict(id_: pl.DataFrame, question: pl.DataFrame, answer: Optional[pl.DataFrame] = None) -> pl.DataFrame:
+    
+    id_value = id_.item(0)
+    question_text = question.item(0)
+    
+    gc.disable()
+    
+    final_answer = solver.solve_problem(question_text)
+    
+    gc.enable()
+    gc.collect()
+    
+    return pl.DataFrame({'id': id_value, 'answer': final_answer})
